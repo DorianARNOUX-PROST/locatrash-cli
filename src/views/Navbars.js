bde5f8034d00d63c1171351a3ca2b5dc8166a351
yohan <yohan.malbranque@gmail.com>
2020-11-19 18:47:42 +0100

    CSS + STATISTIQUES OK

--- src/views/Navbars.js
+++ src/views/Navbars.js
@@ -3,7 +3,9 @@ import React from 'react';
 import { Button, Nav, Form, FormControl } from 'react-bootstrap';
 import '../App.css';
 import '../styles/Navbar.css';
-import { Link, useHistory } from "react-router-dom";
+import '../styles/styles.css';
+import locatrash_banner_alpha from '../assets/locatrash_banner_alpha.png'
+import NavbarBrand from "react-bootstrap/NavbarBrand";
 
 class Navbars extends React.Component{
 
@@ -30,26 +32,38 @@ class Navbars extends React.Component{
       return (
         <div>
           <Navbar fixed="sticky" bg="success" variant="dark">
+            <NavbarBrand href="/">
+              <img src={locatrash_banner_alpha} style={{width:200}} />
+            </NavbarBrand>
             <Nav className="mr-auto">
               <Nav.Link href="/">Accueil</Nav.Link>
               {this.state.loggedIn ?
               <React.Fragment>
                   <Nav.Link href="map">Map</Nav.Link>
                   <Nav.Link href="stats">Statistiques</Nav.Link>
-                  <Nav.Link onClick={() => this.logout()}>Se deconnecter</Nav.Link>
                 </React.Fragment>
                   :
                   <React.Fragment>
-                    <Nav.Link href="signin">Se connecter</Nav.Link>
-                    <Nav.Link href="signup">Creer un compte</Nav.Link>
+
                   </React.Fragment>
                 }
 
+
             </Nav>
-            <Form inline>
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-light">Search</Button>
-            </Form>
+            <div className={"navbar-nav"}>
+            {this.state.loggedIn ?
+
+                <React.Fragment>
+                  <Nav.Link onClick={() => this.logout()}>Se deconnecter</Nav.Link>
+                </React.Fragment>
+                :
+                <React.Fragment>
+                  <Nav.Link href="signin">Se connecter</Nav.Link>
+                  <Nav.Link href="signup">Creer un compte</Nav.Link>
+                </React.Fragment>
+
+            }
+            </div>
           </Navbar>
         </div>
       )
--- src/views/TrashesRepartition.js
+++ src/views/TrashesRepartition.js
@@ -1,115 +1,91 @@
 import React from 'react';
 import axios from 'axios';
-import {Button} from "react-bootstrap";
 import DoughnutChart from "../components/DoughnutChart";
-
-
-function loadData() {
-
-    let route = "http://localhost:8081/trashes/countByCommune";
-    const config = {
-        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
-    };
-    let countByCommune = [];
-    let totalTrashes = 0;
-    try {
-        axios.get(route, config)
-            .then((response) => {
-                response.data.forEach( (element) => {
-                    countByCommune.push({"commune":element.commune, "trashes":element.trashes });
-                    totalTrashes+=element.trashes;
-                });
-                //console.log("total = "+totalTrashes);
-            });
-        let data = [];
-        countByCommune.forEach(item => data.push({
-            title: item.commune,
-            data: [{label: item.commune, value: item.trashes},{label: "Autre", value: totalTrashes-item.trashes}]
-        }));
-        //let data =[];
-        data.push({
-            title: 'Poubelles',
-            data: [{label: "Reste", value: 200},{label: "Lyon", value: 100}]
-        });
-        data.push({
-            title: 'Poubelles2',
-            data: [{label: "Reste", value: 200},{label: "Lyon", value: 100}]
-        });
-        return data;
-        //console.log(data);
-        /*
-        return new Promise(function(resolve, reject) {
-            let script = document.createElement('script');
-            script.src = src;
-
-            script.onload = () => resolve(script);
-            script.onerror = () => reject(new Error(`Script load error for ${src}`));
-
-            document.head.append(script);
-        });;
-
-         */
-    }
-    catch(error){
-        this.setState({error})
-    }
-}
-    function chargeData(){
-        let data = [];
-        console.log(loadData())
-        loadData().forEach(item => data.push({
-            title: item.commune,
-            data: [{label: item.commune, value: item.trashes},{label: "Autre", value: 166}]
-        }));
-        return data;
-    }
-
-    /*
-    let data =[];
-    data.push({
-        title: 'Poubelles',
-        data: [{label: "Reste", value: 200},{label: "Lyon", value: 100}]
-    });
-    data.push({
-        title: 'Poubelles2',
-        data: [{label: "Reste", value: 200},{label: "Lyon", value: 100}]
-    });
-        return data;
-
-     */
-
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import '../styles/styles.css';
 
 class TrashesRepartition extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            data: loadData()
+            data: [],
+            totalT: 0,
+            averageT: 0,
+            minT: 0,
+            maxT: 0
         };
     }
 
-
     componentDidMount() {
-        this.setState({
-            data: loadData()
-        });
+        let route = "http://localhost:8081/trashes/countByCommune";
+        const config = {
+            headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
+        };
+        let countByCommune = [];
+        let totalT = 0;
+        let minT=Number.MAX_SAFE_INTEGER;
+        let maxT =Number.MIN_SAFE_INTEGER;
+        try {
+            axios.get(route, config)
+                .then((response) => {
+                    response.data.forEach( (element) => {
+                        countByCommune.push({"commune":element.commune, "trashes":element.trashes });
+                        totalT+=element.trashes;
+                        if(element.trashes>maxT)maxT=element.trashes;
+                        if(element.trashes<minT)minT=element.trashes;
+                    });
+                    let data = [];
+                    countByCommune.forEach(item => data.push({
+                        title: item.commune,
+                        data: [{label: "Autre", value: totalT-item.trashes},{label: item.commune, value: item.trashes}]
+                    }));
+                    this.setState({ data });
+                    let averageT = (totalT/countByCommune.length).toFixed(2);
+                    this.setState({averageT});
+                    this.setState({totalT});
+                    this.setState({minT});
+                    this.setState({maxT});
+                });
+
+        }
+        catch(error){
+            this.setState({error})
+        }
     }
 
     render() {
         return (
-            <div>
-
+         <Container>
+             <Row>
+                 <Col sm={12} md={6}>
+                     <span className={"title_stats"}>Nombre total de poubelles</span>
+                     <span className={"subtitle_stats"}>{this.state.totalT}</span>
+                     <span className={"title_stats"}>Nombre moyen de poubelles par ville</span>
+                     <span className={"subtitle_stats"}>{this.state.averageT}</span>
+                 </Col>
+                 <Col sm={12} md={6}>
+                     <span className={"title_stats"}>Nombre minimum de poubelles dans une ville</span>
+                     <span className={"subtitle_stats"}>{this.state.minT}</span>
+                     <span className={"title_stats"}>Nombre maximum de poubelles dans une ville</span>
+                     <span className={"subtitle_stats"}>{this.state.maxT}</span>
+                 </Col>
+             </Row>
+            <Row>
                 {this.state.data.map((item, i) => {
                     return (
-                        <div className="sub chart-wrapper ">
+                        <Col sm="6  " md="3" className="sub chart-wrapper ">
                             <DoughnutChart
                                 data={item.data}
                                 title={item.title}
                                 colors={['darkgrey', '#28a745']}
                             />
-                        </div>
+                        </Col>
                         )
                 })}
-            </div>
+            </Row>
+         </Container>
         );
     }
 }
