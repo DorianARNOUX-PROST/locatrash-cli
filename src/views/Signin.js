c27148e42dcbc51a6a953bef711ac5435a944911
yohan <yohan.malbranque@gmail.com>
2020-11-26 12:59:41 +0100

    signin form

--- src/views/Signin.js
+++ src/views/Signin.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
+import {Button, Image} from 'react-bootstrap';
 import axios from 'axios';
+import Form from "react-bootstrap/Form";
+import ViewTitle from "../components/ViewTitle";
+import locatrash_banner2_alpha from "../assets/locatrash_banner2_alpha.png";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "reactstrap/es/Container";
 
 
 class Signin extends Component {
@@ -16,6 +22,10 @@ class Signin extends Component {
         message: ''
       }
     }
+  }
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
   }
 
   signIn() {
@@ -39,25 +49,31 @@ class Signin extends Component {
 
   render() {
     return (
-      <div className="form-inline">
-        <h1>Se connecter</h1>
-        <div className="form-group">
-          <input
-            className="form-control"
-            type="text"
-            placeholder="email"
-            onChange={event => this.setState({email: event.target.value})}
-          />
-          <input
-            className="form-control"
-            type="password"
-            placeholder="mot de passe"
-            onChange={event => this.setState({password: event.target.value})}
-          />
-          <Button variant="dark" onClick={() => this.signIn()}>Se connecter</Button>
-        </div>
-        <div>{this.state.error.message}</div>
-      </div>
+        <Container>
+          <ViewTitle title={"Se connecter"}/>
+          <div className="Login">
+          <Form onSubmit={this.signIn()}>
+            <Form.Group size="lg" controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                  autoFocus
+                  type="text"
+                  placeholder="Email"
+                  onChange={event => this.setState({email: event.target.value})}
+              />
+            </Form.Group>
+            <Form.Group size="lg" controlId="password">
+              <Form.Label>Mot de passe</Form.Label>
+              <Form.Control
+                  type="password"
+                  placeholder="Mot de passe"
+                  onChange={event => this.setState({password: event.target.value})}
+              />
+            </Form.Group>
+            <Button variant="success" block size="lg" type="submit" disabled={!this.validateForm()}>Se connecter</Button>
+          </Form>
+          </div>
+        </Container>
     )
   }
 }
