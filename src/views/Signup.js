af852848e6fce8db4d53142733bbc612a688ca09
Dorian Arnoux <darnouxp.live@gmail.com>
2020-11-26 15:01:06 +0100

    signup form modification

--- src/views/Signup.js
+++ src/views/Signup.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import axios from 'axios';
 import '../styles/form.css';
+import Form from "react-bootstrap/Form";
+import ViewTitle from "../components/ViewTitle";
+import locatrash_banner2_alpha from "../assets/locatrash_banner2_alpha.png";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "reactstrap/es/Container";
 
 
 class Signup extends Component {
@@ -19,6 +25,10 @@ class Signup extends Component {
         message: ''
       }
     }
+  }
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
   }
 
   signUp() {
@@ -43,40 +53,50 @@ class Signup extends Component {
 
   render() {
     return (
-      <div className="form-inline">
-        <h1>Créer un compte</h1>
-        <div className="form-group">
-          <input
-            className="form-control"
-            type="text"
-            placeholder="prénom"
-            onChange={event => this.setState({name: event.target.value})}
-          />
-          <input
-            className="form-control"
-            type="text"
-            placeholder="nom"
-            onChange={event => this.setState({surname: event.target.value})}
-          />
-          <input
-            className="form-control"
-            type="text"
-            placeholder="email"
-            onChange={event => this.setState({email: event.target.value})}
-          />
-          <input
-            className="form-control"
-            type="password"
-            placeholder="mot de passe"
-            onChange={event => this.setState({password: event.target.value})}
-          />
-          <Button variant="dark" onClick={() => this.signUp()}>S'enregistrer</Button>
-          <Button variant="dark" onClick={event =>  window.location.href='/signin'}>J'ai deja un compte</Button>
-
-
-        </div>
-        <div>{this.state.error.message}</div>
-      </div>
+      <Container>
+          <ViewTitle title={"Creer un compte"}/>
+          <div className="Login">
+          <Form onSubmit={this.signUp()}>
+            <Form.Group size="lg" controlId="prenom">
+              <Form.Label>Prénom</Form.Label>
+              <Form.Control
+                  autoFocus
+                  type="text"
+                  placeholder="Prénom"
+                  onChange={event => this.setState({name: event.target.value})}
+              />
+            </Form.Group>
+            <Form.Group size="lg" controlId="nom">
+              <Form.Label>Nom</Form.Label>
+              <Form.Control
+                  autoFocus
+                  type="text"
+                  placeholder="Nom"
+                  onChange={event => this.setState({surname: event.target.value})}
+              />
+            </Form.Group>
+            <Form.Group size="lg" controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                  autoFocus
+                  type="text"
+                  placeholder="Email"
+                  onChange={event => this.setState({email: event.target.value})}
+              />
+            </Form.Group>
+            <Form.Group size="lg" controlId="password">
+              <Form.Label>Mot de passe</Form.Label>
+              <Form.Control
+                  type="password"
+                  placeholder="Mot de passe"
+                  onChange={event => this.setState({password: event.target.value})}
+              />
+            </Form.Group>
+            <Button variant="success" block size="lg" type="submit" disabled={!this.validateForm()}>Creer mon compte</Button>
+            <Button variant="success" block size="lg" onClick={event =>  window.location.href='/signin'}>J'ai deja un compte</Button>
+          </Form>
+          </div>
+        </Container>
     )
   }
 }
